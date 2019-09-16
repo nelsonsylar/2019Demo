429da08683f7e88cdf02816a7642f2f62ac5a0fe
Nelson <a2135825@gmail.com>
2019-09-16 14:55:54 +0800

    restore

--- Vue-demo2/youzan/README.md
+++ Vue-demo2/youzan/README.md
@@ -1,5 +1,9 @@
 # youzan
 
-> youzan store
+> 仿使用vue-cli搭建有赞商城前端页面
 
++ 会记录在这过程中遇到的坑
 + vue cli 工具默认搭建的是单页面应用模板,需要改为多页面应用模板
++ 修改webpack alias配置方便使用简写
++ 在使用for in 遍历对象时,切记一定要用 obj[key]的形式调用对象的属性,而不能使用obj.key
+
--- Vue-demo2/youzan/src/pages/index/index.js
+++ Vue-demo2/youzan/src/pages/index/index.js
@@ -1,15 +1,35 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import './index.css'
+import 'css/common.css'
+import axios from 'axios'//注意导入都是小写,且需要用引号括起来
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+import url from 'js/api.js'
+// import foot from 'componens/foot.vue'
+//注释为学习笔记
+//这里由于我在webpack配置文件写了alias
+//'css':'@/modules/css/',
+//'js':'@/modules/js/',
+//另外一切皆为模块,css由js导入,并且webpack的配置会识别相应文件夹的相同名.js并自动加载
+//所以此index.js为入口
 
-Vue.config.productionTip = false
-
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+//vue导入并且需要new Vue作为入口,并以el:'#xxx'挂载,并且范围要小于html的body
+//axios需要先安装再导入
+var app = new Vue({
+    el:'#app',
+    data:{
+        lists:null
+    },
+    created(){
+        
+        axios.get(url.hotList,{
+            pageNum:0,
+            pageSize:6,
+        }).then((response)=>{
+            this.lists=response.data.lists
+        }).catch((error)=>{
+            console.log('Fail to get data'+error)
+        })
+    },
+    // components:{
+    //     foot
+    // }
 })
