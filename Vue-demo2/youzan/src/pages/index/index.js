f27465615f78a28705c16f1530261a26d8bcaa1e
Sylar <449574189@qq.com>
2019-09-16 18:39:34 +0800

    mint-UI遇坑

--- Vue-demo2/youzan/src/pages/index/index.js
+++ Vue-demo2/youzan/src/pages/index/index.js
@@ -3,6 +3,9 @@ import 'css/common.css'
 import axios from 'axios'//注意导入都是小写,且需要用引号括起来
 import Vue from 'vue'
 import url from 'js/api.js'
+import { InfiniteScroll } from 'mint-ui';
+
+Vue.use(InfiniteScroll);
 // import foot from 'componens/foot.vue'
 //注释为学习笔记
 //这里由于我在webpack配置文件写了alias
@@ -16,20 +19,52 @@ import url from 'js/api.js'
 var app = new Vue({
     el:'#app',
     data:{
-        lists:null
+        lists:null,
+        pageNum:1,
+        pageSize:6,
+        loading:false,
+        allLoaded:false,
     },
     created(){
-        
-        axios.get(url.hotList,{
-            pageNum:0,
-            pageSize:6,
-        }).then((response)=>{
-            this.lists=response.data.lists
-        }).catch((error)=>{
-            console.log('Fail to get data'+error)
-        })
+        this.getHotlist()
     },
+    methods:{
+        getHotlist(){
+                axios.get(url.hotList,{
+                    pageNum:this.pageNum,
+                    pageSize:this.pageSize
+                }).then((response)=>{
+                    this.lists= response.data.lists
+                    console.log('执行了一次')
+                })
+        }
+    }
+            // if(this.allLoaded===true) return  
+                // axios.get(url.hotList,{
+                //     pageNum:this.pageNum,
+                //     pageSize:this.pageSize,
+                // }).then((response)=>{
+                //     let currentList=response.data.lists
+                //     if(this.lists){
+                //         this.lists= this.lists.concat(currentList)
+                //         console.log('走的1')
+                //     }else{
+                //         this.lists=currentList
+                //         console.log('走的2')
+                //     }
+                //     this.pageNum++
+                //     // this.loading=true
+                // }).catch((error)=>{
+                //     console.log('Fail to get data'+error)
+                // })
+                // this.pageNum++
+                // this.loading=true
+                  
+        
+
+    
     // components:{
     //     foot
     // }
+
 })
